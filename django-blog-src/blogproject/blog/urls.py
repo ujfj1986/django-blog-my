73e66e26d24565b9abde3944222c402ef23754d6
Jiejing Shan <ujfj1986@163.com>
2017-11-08 01:16:40 +0800

    1. Modify templates index.html. 2. fix get post bug. 3. Add url 'index.html' process. 4. git rm 2.

--- django-blog-src/blogproject/blog/urls.py
+++ django-blog-src/blogproject/blog/urls.py
@@ -6,4 +6,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^index.html$', views.index, name='index'),
+
 ]
--- django-blog-src/blogproject/blog/views.py
+++ django-blog-src/blogproject/blog/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 from .models import Post
 
 def index(request):
-    post_list = Post.objects.all().order_by('-create_time')
+    post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blog/index.html', context= {
             'post_list': post_list})
