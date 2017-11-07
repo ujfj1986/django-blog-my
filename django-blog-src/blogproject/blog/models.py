9c6052da0072cf405670c862d8dfc0d1fe78c92e
Jiejing Shan <ujfj1986@163.com>
2017-11-08 00:42:05 +0800

    Modify blog models.
    1. add python2 unicode decorator.
    2. create super user.

--- django-blog-src/blogproject/blog/models.py
+++ django-blog-src/blogproject/blog/models.py
@@ -2,9 +2,12 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.six import python_2_unicode_compatible
 
 # Create your models here.
 
+# python_2_unicode_compatible 装饰器用于兼容 Python2
+@python_2_unicode_compatible
 class Category(models.Model):
     """
     Django 要求模型必须继承 models.Model 类。
@@ -17,6 +20,10 @@ class Category(models.Model):
     """
     name = models.CharField(max_length = 100)
 
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible
 class Tag(models.Model):
     """
     标签 Tag 也比较简单，和 Category 一样。
@@ -24,7 +31,11 @@ class Tag(models.Model):
     """
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
 
+
+@python_2_unicode_compatible
 class Post(models.Model):
     """
     文章的数据库表稍微复杂一点，主要是涉及的字段更多。
@@ -61,6 +72,10 @@ class Post(models.Model):
     # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和 Category 类似。
     author = models.ForeignKey(User)
 
+    def __str__(self):
+        return self.title
+
+@python_2_unicode_compatible
 class Comment(models.Model):
     """
     文章的评论
@@ -81,3 +96,6 @@ class Comment(models.Model):
 
     #评论可以是另外一个评论的回复
     commentid = models.IntegerField(blank=True)
+
+    def __str__(self):
+        return self.title
