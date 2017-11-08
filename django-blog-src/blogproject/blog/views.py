b0274ef9cc7fa964486d2755761feb8d4524375b
Jiejing Shan <ujfj1986@163.com>
2017-11-09 00:23:27 +0800

    Show post according to category/tag/archive.

--- django-blog-src/blogproject/blog/views.py
+++ django-blog-src/blogproject/blog/views.py
@@ -3,7 +3,7 @@ import markdown
 
 # Create your views here.
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Category, Tag
 
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
@@ -19,3 +19,22 @@ def detail(request, pk):
                                      'markdown.extensions.toc',
                                  ])
     return render(request, 'blog/detail.html', context={'post': post})
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year = year,
+                                    created_time__month = month
+                                   ).order_by('-created_time')
+    return render(request, 'blog/index.html', context= {
+            'post_list': post_list})
+
+def get_post_by_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=category).order_by('-created_time')
+    return render(request, 'blog/index.html', context= {
+            'post_list': post_list})
+
+def get_posts_by_tag(request, pk):
+    tag = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tags=tag).order_by('-created_time')
+    return render(request, 'blog/index.html', context={
+            'post_list': post_list})
