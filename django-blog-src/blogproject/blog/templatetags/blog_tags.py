455d201fc1ef98f1cc13db873a18412fa8c384e6
shanjiejing <shanjiejing@letv.com>
2017-11-09 16:38:04 +0800

    1. Modify some jump function. 2. show the right comments number. 3. show the archive's post number. 4. show comments of post ordered by reverse created time.

--- django-blog-src/blogproject/blog/templatetags/blog_tags.py
+++ django-blog-src/blogproject/blog/templatetags/blog_tags.py
@@ -7,11 +7,16 @@ register = template.Library()
 
 @register.simple_tag
 def get_recent_posts(num = 5):
-    return Post.objects.all().order_by('-created_time')[:num]
+    return Post.objects.all()[:num]
 
 @register.simple_tag
 def archives():
     return Post.objects.dates('created_time', 'month', order='DESC')
+
+@register.simple_tag
+def get_post_num_by_archive(year, month):
+    return Post.objects.filter(created_time__year = year,
+                            created_time__month = month).count()
 
 @register.simple_tag
 def get_categories():
