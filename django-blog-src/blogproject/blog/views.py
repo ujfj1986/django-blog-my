7ddd4c103a064c827cb7aff9f42db04b4548a0a3
Jiejing Shan <ujfj1986@163.com>
2017-11-13 23:32:31 +0800

    fix before/next post bug.

--- django-blog-src/blogproject/blog/views.py
+++ django-blog-src/blogproject/blog/views.py
@@ -80,7 +80,9 @@ class PostDetailView(DetailView, BlogBasicView):
         # get 方法返回的是一个 HttpResponse 实例
         # 之所以需要先调用父类的 get 方法，是因为只有当 get 方法被调用后，
         # 才有 self.object 属性，其值为 Post 模型实例，即被访问的文章 post
+        #logger.error("in PostDetailView.get.")
         response = super(PostDetailView, self).get(request, *args, **kwargs)
+        #logger.error("after supper")
 
         # 将文章阅读量 +1
         # 注意 self.object 的值就是被访问的文章 post
@@ -90,21 +92,33 @@ class PostDetailView(DetailView, BlogBasicView):
         # 视图必须返回一个 HttpResponse 对象
         return response
 
+    '''def get_queryset(self):
+        logger.error("in get_queryset.")
+        return super(DetailView, self).get_queryset()'''
+
     def get_object(self, queryset=None):
         # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+        #logger.error("in get_object, %s." % queryset)
         post = super(PostDetailView, self).get_object(queryset=None)
+        #logger.error("after get_object super. post is %s." % Post)
         post.body = markdown.markdown(post.body,
                                       extensions=[
                                           'markdown.extensions.extra',
                                           'markdown.extensions.codehilite',
                                           'markdown.extensions.toc',
                                       ])
+        pk = self.kwargs.get('pk')
+        #logger.error("pk is %s." % pk)
+        post.before = Post.objects.filter(pk=str(int(pk)-1)).first()
+        post.next = Post.objects.filter(pk=str(int(pk)+1)).first()
         return post
 
     def get_context_data(self, **kwargs):
         # 覆写 get_context_data 的目的是因为除了将 post 传递给模板外（DetailView 已经帮我们完成），
         # 还要把评论表单、post 下的评论列表传递给模板。
+        #logger.error("in get_context_data." )
         context = super(PostDetailView, self).get_context_data(**kwargs)
+        #logger.error("after get_context_data super.")
         form = CommentForm()
         comment_list = self.object.comment_set.all()
         context.update({
