394b816848c46b633af86370d915fe81df78306f
shanjiejing <shanjiejing@letv.com>
2017-11-16 16:26:00 +0800

    implement blog modify/add function. 80%.

--- django-blog-src/blogproject/blog/views.py
+++ django-blog-src/blogproject/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 import markdown
 
 # Create your views here.
@@ -8,6 +8,7 @@ from comment.forms import CommentForm
 from .blogsession import BlogSession
 from .forms import PostForm
 from django.utils.timezone import now
+from django.contrib.auth.models import User
 import logging
 
 logger = logging.getLogger('blog.views')
@@ -198,6 +199,9 @@ class PostModifyView(DetailView, BlogBasicView):
             #form.fields['title'] = post.title + 'ddd'
             form.pk = post.pk
             #form.init(post)
+            form.is_modify = True
+        else :
+            form.is_modify = False
         return form
 
     def get_context_data(self, **kwargs):
@@ -208,6 +212,7 @@ class PostModifyView(DetailView, BlogBasicView):
 
     def post(self, request, *args, **kwargs):
         self.blogsession.update(request)
+        logger.error("user is %s." % self.blogsession.username)
         cur_path=request.get_full_path()
         if not self.blogsession.isLogined:
             return HttpResponseRedirect('/login/next=%s' % cur_path)
@@ -221,7 +226,9 @@ class PostModifyView(DetailView, BlogBasicView):
         else:
             post = Post()
             post.created_time = now()
-            post.author = User.object.get(username=self.blogsession.username)
+            user = User.objects.get(username=self.blogsession.username)
+            logger.error("--- user is %s." % user.username)
+            post.author = user
         post.title = form.cleaned_data['title']
         post.excerpt = form.cleaned_data['excerpt']
         cate = Category.objects.all().filter(name=form.cleaned_data['category']).first()
@@ -229,14 +236,34 @@ class PostModifyView(DetailView, BlogBasicView):
             cate = Category(name=form.cleaned_data['category'])
             cate.save()
         post.category = cate
-        tags_str = form.cleaned_data['tags']
-        tags = None
-        for tag in tags_str.split(' '):
-            tags = Tag.objects.all().filter(name=tag).first()
-            if tags is None:
-                tags = Tag(name=tag)
-                tags.save()
-            post.tags.push(tags)
         post.body = form.cleaned_data['body']
+        post.save()
+        tags_str = form.cleaned_data['tags']
+        old_tags = []
+        if post.tags is not None:
+            old_tags = post.tags.all()
+        logger.error("post new tags: %s." % tags_str)
+        logger.error("post old tags:")
+        for tag in old_tags:
+            logger.error(tag.name)
+        tag_obj = None
+        for tag in tags_str.split(' '):
+            if tag is None:
+                continue
+            logger.error("tag is %s." % tag)
+            tag_obj = Tag.objects.all().filter(name=tag).first()
+            if tag_obj is None:
+                tag_obj = Tag(name=tag)
+                tag_obj.save()
+            if post.tags is None:
+                post.tags.create(tag_obj)
+                continue
+            if tag_obj in post.tags.all():
+                continue
+            post.tags.add(tag_obj)
+        for tag in old_tags:
+            if tag.name in tags_str:
+                continue
+            post.tags.remove(tag)
         post.save()
         return redirect(post)
