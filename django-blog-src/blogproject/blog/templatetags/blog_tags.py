543c5f04c3c630658e2309281147efd56edfe2c1
Jiejing Shan <ujfj1986@163.com>
2017-11-17 01:44:19 +0800

    Another method to implement getting the same category/tag posts number.

--- django-blog-src/blogproject/blog/templatetags/blog_tags.py
+++ django-blog-src/blogproject/blog/templatetags/blog_tags.py
@@ -2,6 +2,7 @@
 
 from ..models import Post, Category, Tag
 from django import template
+from django.db.models.aggregates import Count
 
 register = template.Library()
 
@@ -20,8 +21,8 @@ def get_post_num_by_archive(year, month):
 
 @register.simple_tag
 def get_categories():
-    return Category.objects.all()
+    return Category.objects.annotate(num_posts = Count('post')).filter(num_posts__gt = 0)
 
 @register.simple_tag
 def get_tags():
-    return Tag.objects.all()
+    return Tag.objects.annotate(num_posts = Count('post'))
